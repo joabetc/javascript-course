8bccc51b8495e4da467c1dabebe2a9bf83af138c
Joabe Costa <joabetc@hotmail.com>
2018-01-07 17:33:53 -0200

    Preparing formula for calculating BMI

--- js/principal.js
+++ js/principal.js
@@ -9,8 +9,5 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
-console.log(peso);
-console.log(tdAltura);
-console.log(altura);
+var imc = peso / altura * altura;
+console.log(imc);
