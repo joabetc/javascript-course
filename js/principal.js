9e5305e2e875515f2a0984ef3a35f555bfcd07b8
Joabe Costa <joabetc@hotmail.com>
2018-01-07 17:30:18 -0200

    Getting td value for weight

--- js/principal.js
+++ js/principal.js
@@ -2,4 +2,10 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
 console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
