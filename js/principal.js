ebeb872f65c426bc22be4e9816413d013a07cbc5
Joabe Costa <joabetc@hotmail.com>
2018-01-07 17:53:35 -0200

    Preventing calculation when values are not valid

--- js/principal.js
+++ js/principal.js
@@ -11,13 +11,18 @@ var altura = tdAltura.textContent;
 
 var tdIMC = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+
 if (peso < 0 || peso > 1000) {
-    console.log("Peso inválido");
+    pesoEhValido = false;
 }
 
 if (altura < 0 || altura > 3) {
-    console.log("Altura inválida");
+    alturaEhValida = false;
 }
 
-var imc = peso / (altura * altura);
-tdIMC.textContent = imc;
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdIMC.textContent = imc;
+}
