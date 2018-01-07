e4e99a7b4aa5c0321db464f96e5a4db0485e5f15
Joabe Costa <joabetc@hotmail.com>
2018-01-07 17:31:55 -0200

    Getting td value for height

--- js/principal.js
+++ js/principal.js
@@ -2,10 +2,15 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
 
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
 console.log(paciente);
 console.log(tdPeso);
 console.log(peso);
+console.log(tdAltura);
+console.log(altura);
