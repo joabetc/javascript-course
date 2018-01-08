4d6356981066a1ff8482531ce68fedd1db8acc5a
Joabe Costa <joabetc@hotmail.com>
2018-01-07 23:30:15 -0200

    Adjusting validation condition

--- js/principal.js
+++ js/principal.js
@@ -14,13 +14,13 @@ var tdIMC = paciente.querySelector(".info-imc");
 var pesoEhValido = true;
 var alturaEhValida = true;
 
-if (peso <= 0 || peso > 1000) {
+if (peso <= 0 || peso >= 1000) {
     console.log("Peso inválido");
     pesoEhValido = false;
     tdIMC.textContent = "Peso inválido";
 }
 
-if (altura <= 0 || altura > 3) {
+if (altura <= 0 || altura >= 3) {
     console.log("Altura inválida");
     alturaEhValida = false;
     tdIMC.textContent = "Altura inválida";
