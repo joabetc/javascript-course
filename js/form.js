bd03e83a45474c5414e352d59dc9eda8d5f0d9e1
Joabe Costa <joabetc@hotmail.com>
2018-01-09 21:20:43 -0200

    Using form reset to clean form fields

--- js/form.js
+++ js/form.js
@@ -11,6 +11,8 @@ botaoAdicionar.addEventListener("click", function(event) {
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTR);
+
+    form.reset();
 });
 
 function obtemPaciente(form) {
