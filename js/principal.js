486dfcf6d5545ebfbbda09b3fb27882fc7adca10
Joabe Costa <joabetc@hotmail.com>
2018-01-08 23:20:08 -0200

    Adding an event listener to the form button

--- js/principal.js
+++ js/principal.js
@@ -38,6 +38,7 @@ for(var i = 0; i < trPacientes.length; i++) {
     }
 }
 
-titulo.addEventListener("click", function() {
-    console.log("Olha só posso chamar uma função anônima.");
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function() {
+    console.log("Oi, cliquei no botão");
 });
