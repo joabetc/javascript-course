db9a3bf2ea767fdc0e8527a6492c8e284e1d6c67
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-01-10 12:54:27 -0200

    Using setTimeout and adding animation to item removing

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,8 @@
 var table = document.querySelector("table");
 
 table.addEventListener("dblclick", function(event) {
-  event.target.parentNode.remove();
+  event.target.parentNode.classList.add("fadeOut");
+  setTimeout(function() {
+    event.target.parentNode.remove();
+  }, 500);
 })
