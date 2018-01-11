f5bc11c7e303579bb4dd2ba8644cd785f0564098
Joabe Costa <joabetc@hotmail.com>
2018-01-10 23:23:10 -0200

    Implementing filter function

--- js/filtra.js
+++ js/filtra.js
@@ -5,6 +5,11 @@ campoFiltro.addEventListener("input", function() {
     for(var i = 0; i < pacientes.length; i++) {
         var paciente = pacientes[i];
         var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
+        var nome = tdNome.textContent;
+        if (nome != this.value) {
+            paciente.classList.add("invisivel");
+        } else {
+            paciente.classList.remove("invisivel");
+        }
     }
 })
