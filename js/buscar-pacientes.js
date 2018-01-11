9c9efe26b094256467d28364c34a0fcb6715b1c3
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-01-11 13:04:35 -0200

    Checking response status

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,11 +6,17 @@ botaoAdicionar.addEventListener("click", function() {
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
   xhr.addEventListener("load", function() {
-    var resposta = xhr.responseText;
-    var pacientes = JSON.parse(resposta);
-    pacientes.forEach(function(paciente) {
-      adicionaPacienteNaTabela(paciente);
-    });
+
+    if (xhr.status == 200) {
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+      pacientes.forEach(function(paciente) {
+        adicionaPacienteNaTabela(paciente);
+      });
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+    }
   });
 
   xhr.send();
