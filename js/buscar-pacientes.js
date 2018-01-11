55fc361090c53705871276723aad54d80f1a21bd
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-01-11 13:11:46 -0200

    Showing error message when request fails

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,8 +6,9 @@ botaoAdicionar.addEventListener("click", function() {
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
   xhr.addEventListener("load", function() {
-
+    var erroAjax = document.querySelector("#erro-ajax");
     if (xhr.status == 200) {
+      erroAjax.classList.add("invisivel");
       var resposta = xhr.responseText;
       var pacientes = JSON.parse(resposta);
       pacientes.forEach(function(paciente) {
@@ -16,6 +17,7 @@ botaoAdicionar.addEventListener("click", function() {
     } else {
       console.log(xhr.status);
       console.log(xhr.responseText);
+      erroAjax.classList.remove("invisivel");
     }
   });
 
